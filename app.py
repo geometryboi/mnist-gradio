f72e2db11e0974ba31563339d76235f096cafd58
geometryboi <01234wasdwasd56789@gmail.com>
2026-05-20 22:57:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from bootstrap.container import Container
 
-def main():
-    app = Container().build_app()
-    app.launch()
-
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    app = Container().build_app()
+    app.launch(server_name="0.0.0.0", server_port=7860)
